719979ef21b05e4eb93e665becf292edf2b28b7e
dgstephenson <dgstephenson.econ@gmail.com>
2024-08-23 16:55:28 -0400

    both sessions

--- 3-regression.R
+++ 3-regression.R
@@ -1,17 +1,22 @@
 df = read.csv('clean/cleanData.csv',row.names = NULL)
 studies = c('66be300552fc3c98bcdc6c35','66c8d0c8779c5adea7f68954')
-df = df[df$study==studies[2],]
+study1 = df$study == studies[1]
+study2 = df$study == studies[2]
 
-lowerBound = 0.1
-upperBound = 0.4
+#df = df[study2,]
+
+lowerBound = 1/5*0.5
+upperBound = 4/5*0.5
 interiorChoice1 = lowerBound <= df$choice1 & df$choice1 <= upperBound
+lowChoice1 = df$choice1 < lowerBound
+highChoice1 = df$choice1 > upperBound
 
 summary(lm(choice2 ~ score1, data=df[interiorChoice1,]))
 print(sum(interiorChoice1))
 
-summary(lm(choice2 ~ score1, data=df[df$choice1 < lowerBound,]))
-print(sum(df$choice1 < lowerBound))
+summary(lm(choice2 ~ score1, data=df[lowChoice1,]))
+print(sum(lowChoice1))
 
-summary(lm(choice2 ~ score1, data=df[df$choice1 > upperBound,]))
-print(sum(df$choice1 > upperBound))
+summary(lm(choice2 ~ score1, data=df[highChoice1,]))
+print(sum(highChoice1))
 
